e4674cbf0c29e05b9e729760b0f173c6f2bee775
Virtlink <647530+Virtlink@users.noreply.github.com>
2018-07-15 16:49:30 +0200

    Create initial window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 extern crate ash;
+extern crate winit;
+
+use winit::{ControlFlow, Event, VirtualKeyCode, WindowEvent};
 
 fn main() {
-    println!("Hello, world!");
+    println!("Starting...");
+
+    let mut events_loop = winit::EventsLoop::new();
+    let _window = winit::WindowBuilder::new()
+        .with_title("Test Window")
+        .build(&events_loop)
+        .unwrap();
+
+    events_loop.run_forever(|event| match event {
+        Event::WindowEvent { event, .. } => match event {
+            WindowEvent::KeyboardInput { input, .. } => {
+                if let Some(VirtualKeyCode::Escape) = input.virtual_keycode {
+                    ControlFlow::Break
+                } else {
+                    ControlFlow::Continue
+                }
+            }
+            WindowEvent::CloseRequested => winit::ControlFlow::Break,
+            _ => ControlFlow::Continue,
+        },
+        _ => ControlFlow::Continue,
+    });
+
+    println!("Started!");
 }
