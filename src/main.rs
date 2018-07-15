9430582e7471b8f07d830d213797c5bfabb7d7e8
Virtlink <647530+Virtlink@users.noreply.github.com>
2018-07-15 19:06:31 +0200

    Print Vulkan extensions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 extern crate ash;
+extern crate core;
 extern crate winit;
 
+use ash::version::EntryV1_0;
+use ash::vk;
+use ash::Entry;
+use core::slice;
 use winit::{ControlFlow, Event, VirtualKeyCode, WindowEvent};
 
 fn main() {
@@ -11,6 +16,13 @@ fn main() {
         .with_title("Test Window")
         .build(&events_loop)
         .unwrap();
+
+    let entry = Entry::new().unwrap();
+    let extensions = entry.enumerate_instance_extension_properties().unwrap();
+    println!("{} extensions supported:", extensions.len());
+    for extension in extensions {
+        println!("{}", array_to_utf8(&extension.extension_name));
+    }
 
     events_loop.run_forever(|event| match event {
         Event::WindowEvent { event, .. } => match event {
@@ -28,4 +40,9 @@ fn main() {
     });
 
     println!("Started!");
+}
+
+fn array_to_utf8(i8slice: &[i8]) -> &str {
+    let u8slice = unsafe { slice::from_raw_parts(i8slice.as_ptr() as *const u8, i8slice.len()) };
+    return std::str::from_utf8(u8slice).unwrap();
 }
